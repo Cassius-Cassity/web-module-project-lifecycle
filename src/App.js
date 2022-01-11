4c0cc5f3999df6c823c8d4f453d3045650990f3b
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2022-01-11 16:53:25 -0700

    laying the foundation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import axios from 'axios';
 class App extends React.Component {
+
+state = {
+  avatar: "",
+  name: '',
+  followers: '',
+}
+
+componentDidMount(){
+  axios.get('https://api.github.com/users/Cassius-Cassity')
+  .then(resp => {
+    this.setState({
+      ...this.state,
+      avatar: resp.data.avatar_url,
+      name: resp.data.name,
+      followers: resp.data.followers
+    })
+  })
+}
   render() {
     return(<div>
-      Github Card
+      <h1>Github Card</h1>
+      <h3>Name: {this.state.name}</h3>
+      <img src={this.state.avatar}/>
+      <p>followers: {this.state.followers}</p>
     </div>);
   }
 }
